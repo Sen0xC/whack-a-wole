dcfe7d296faaee06cb04d1883ededd6ae972c795
Sen0xC <romerocyril8@gmail.com>
2023-12-09 00:31:00 +0100

    functionnal game

--- code/mole.js
+++ code/mole.js
@@ -1,70 +1,105 @@
-// Déclaration des variables globales pour la tuile courante de la taupe et de la tuile courante du trou
+// Declaration of global variables for the current mole tile and the current hole tile
 let currMoleTile;
 let currHoleTile;
-let score= 0;
+let score = 0;
 let gameOver = false;
 
-// La fonction se déclenche lorsque la fenêtre est complètement chargée
+// The function triggers when the window is fully loaded
 window.onload = function() {
     setGame();
 }
 
-// Fonction pour initialiser le jeu
+// Function to initialize the game
 function setGame(){
-    // Création de 9 tuiles sur le plateau
+    // Creating 9 tiles on the board
     for (var i = 0; i < 9 ; i++) {
         let tile = document.createElement("div");
         tile.id = i.toString();
+        tile.addEventListener("click", selectTile);
         document.getElementById("board").appendChild(tile);
     }
 
-    // Lancement des intervalles pour afficher une taupe toutes les 1500 ms et un trou toutes les 2500 ms
-    setInterval(setMole, 1500);
-    setInterval(setHole, 2500);
+    // Starting intervals to display a mole every 1500 ms and a hole every 2500 ms
+    // Adjusting intervals dynamically based on the score
+    if (score > 500) {
+        setInterval(setMole, 500);
+        setInterval(setHole, 1000);
+    } else {
+        setInterval(setMole, 1500);
+        setInterval(setHole, 2500);
+    }
 }
 
-// Fonction pour obtenir un numéro de tuile aléatoire
+// Function to get a random tile number
 function getRandomTile(){
     return Math.floor(Math.random() * 9).toString();
 }
 
-// Fonction pour afficher un trou sur une tuile aléatoire
+// Function to display a hole on a random tile
 function setHole(){
-    // Si une tuile de trou précédente existe, la vider
+    // Skip if the game is over
+    if(gameOver){
+        return;
+    }
+
+    // If a previous hole tile exists, clear it
     if (currHoleTile){
         currHoleTile.innerHTML = "";
     }
 
-    // Création d'une image de trou
+    // Creating a hole image
     let hole = document.createElement("img");
     hole.src = "../img/hole-in-the-ground-free-png.png";
 
-    // Obtenir un numéro de tuile aléatoire et ajouter le trou à la tuile correspondante
+    // Get a random tile number and add the hole to the corresponding tile
     let num = getRandomTile();
     currHoleTile = document.getElementById(num);
     currHoleTile.appendChild(hole);
 }
 
-// Fonction pour afficher une taupe sur une tuile aléatoire
+// Function to display a mole on a random tile
 function setMole(){
-    // Si une tuile de taupe précédente existe, la vider
+    // Skip if the game is over
+    if(gameOver){
+        return;
+    }
+
+    // If a previous mole tile exists, clear it
     if (currMoleTile){
         currMoleTile.innerHTML = "";
     }
 
-    // Création d'une image de taupe
+    // Creating a mole image
     let mole = document.createElement("img");
     mole.src = "../img/mole.png";
 
-    // Obtenir un numéro de tuile aléatoire
+    // Get a random tile number
     let num = getRandomTile();
 
-    // Vérifier si le trou est occupé par une taupe avant de placer une nouvelle taupe
+    // Check if the hole is occupied by a mole before placing a new mole
     if (currHoleTile && currHoleTile.id == num){
         return;
     }
 
-    // Ajouter la taupe à la tuile correspondante
+    // Add the mole to the corresponding tile
     currMoleTile = document.getElementById(num);
     currMoleTile.appendChild(mole);
+}
+
+// Function to handle tile selection
+function selectTile(){
+    // Skip if the game is over
+    if(gameOver){
+        return;
+    }
+
+    // Check if the selected tile matches the mole or hole tile
+    if(this == currMoleTile){
+        score += 10;
+        document.getElementById("score").innerText = score.toString();
+    }
+    else if(this == currHoleTile){
+        document.getElementById("score").innerText = "Game Over: " + score.toString();
+        gameOver = true;
+    }
 }
